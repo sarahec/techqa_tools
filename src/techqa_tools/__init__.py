d4c884f76b87298efd4079687eb707ac63fd4bba
sarahec <sarahec@nextquestion.net>
2024-03-12 07:08:22 -0700

    Export required

--- src/techqa_tools/__init__.py
+++ src/techqa_tools/__init__.py
@@ -1,2 +1,2 @@
 from .tools import read_corpus_entries, read_corpus_segments, save_tokenized_corpus, tokenize_corpus, label_corpus
-from .commands import tokenize
+from .commands import tokenize, label_text
